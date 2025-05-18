14036c5ed0047749c086e0cc9c3c4217369182e7
yoshzucker <170805209+yoshzucker@users.noreply.github.com>
2025-05-19 06:56:18 +0900

    feat(obsidian): add obsidian-mode to my-app-misc.el

---
+++ emacs.d/modules/my-app-misc.el
@@ -0,0 +1,27 @@
+;;; my-app-misc.el --- Miscellaneous App integrations -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; This file handles integrations with external applications like Obsidian.
+
+;;; Code:
+
+(use-package obsidian
+  :init
+  (setq obsidian-directory
+        (expand-file-name "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/complexvault/"))
+  :config
+  (my/define-key
+   (:map obsidian-mode-map
+         :prefix "C-c"
+         :key
+         "C-n" #'obsidian-capture
+         "C-l" #'obsidian-insert-link
+         "C-o" #'obsidian-follow-link-at-point
+         "C-p" #'obsidian-jump
+         "C-n" #'obsidian-jump-back
+         "C-b" #'obsidian-backlink-jump))
+
+  (global-obsidian-mode 1))
+
+(provide 'my-app-misc)
+;;; my-app-misc.el ends here
